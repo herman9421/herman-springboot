50e4daab06f9b12a4bbbdfa5fee532fe00232da3
herman <wanghenan3111@163.com>
2019-03-14 09:52:00 +0800

    提交springboot项目

---
+++ src/main/java/com/herman/springboot/SpringbootApplication.java
@@ -0,0 +1,27 @@
+package com.herman.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: herman
+ * @Description:
+ * @Date: 2019/3/13
+ */
+@RestController
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world";
+    }
+
+}
